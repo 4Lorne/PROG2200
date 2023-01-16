c4e6edff9c2785fe6a4991e62eb26ff20d8d5c05
4Lorne <lornedayy@gmail.com>
2023-01-16 13:15:36 -0400

    Restructured project folders

---
+++ Lab1/Module 1/Section 1/Survey/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Survey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("What is your age?");
+            var age = Console.ReadLine();
+
+            Console.WriteLine("What month were you born in?");
+            var month = Console.ReadLine();
+
+            Console.WriteLine("What city were you born in?");
+            var city = Console.ReadLine();
+
+            Console.WriteLine("How many siblings do you have?");
+            var siblings = Console.ReadLine();
+            
+
+            Console.WriteLine("Your name is: {0}", name);
+            Console.WriteLine("Your age is: {0}", age);
+            Console.WriteLine("Your birth month is: {0}", month);
+            Console.WriteLine("You were born in: {0}",city);
+            Console.WriteLine("You have {0} siblings",siblings);
+        }
+    }
+}
