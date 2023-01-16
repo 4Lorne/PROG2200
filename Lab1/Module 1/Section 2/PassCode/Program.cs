48336e4b590e01f659075bc8716debe466c9f72a
4Lorne <lornedayy@gmail.com>
2023-01-16 13:32:59 -0400

    Module 1 Section 2 Complete Used switch case instead of if/else

---
+++ Lab1/Module 1/Section 2/PassCode/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace PassCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var code = "";
+            var realCode = "secret";
+            while (code != realCode)
+            {
+                Console.WriteLine("What is the pass code?");
+                code = Console.ReadLine();
+                switch (code)
+                {
+                    case "secret":
+                        Console.WriteLine("Authenticated");
+                        break;
+                    default:
+                        Console.WriteLine("Not Authenticated");
+                        break;
+                }
+            }
+
+            Console.WriteLine("Would you like to change the password? Y/N");
+            var answer = Console.ReadLine();
+
+            if (!answer.Equals("Y",StringComparison.CurrentCultureIgnoreCase)) return;
+            Console.WriteLine("What is the new password?");
+            realCode = Console.ReadLine();
+
+            Console.WriteLine("Password is updated.");
+        }
+    }
+}
