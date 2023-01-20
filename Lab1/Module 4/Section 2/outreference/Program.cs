73efc37fe8b8ce94337f31b559a2a882022077ee
4Lorne <lornedayy@gmail.com>
2023-01-20 14:40:56 -0400

    Section 1 and 2 Complete

---
+++ Lab1/Module 4/Section 2/outreference/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace outreference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 2;
+
+            Console.WriteLine(x);
+
+            Five(out x);
+
+            Console.WriteLine(x);
+
+            var test = "1.5";
+            var outDouble = 0.0;
+            Console.WriteLine("Result: {0}", parseDouble(test, out outDouble));
+            Console.WriteLine("Test type is {0}", outDouble.GetType());
+            test = "Ronan";
+            outDouble = 0.0;
+            Console.WriteLine("Result: {0}", parseDouble(test, out outDouble));
+            Console.WriteLine("Test value is {0}", outDouble);
+
+            
+            
+        }
+
+        static void Five(out int a)
+        {
+            a = 5;
+            Console.WriteLine(a);
+        }
+
+        static bool parseDouble(string input, out double output)
+        {
+            var outputBool = double.TryParse(input, out output);
+            return outputBool;
+        }
+    }
+}
