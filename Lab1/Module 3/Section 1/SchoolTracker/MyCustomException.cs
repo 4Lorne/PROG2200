a097dce023e904a05ea4c30e18dc7cc791ee778c
4Lorne <lornedayy@gmail.com>
2023-01-20 12:45:56 -0400

    Custom Exception done

---
+++ Lab1/Module 3/Section 1/SchoolTracker/MyCustomException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SchoolTracker
+{
+    public class MyCustomException : FormatException
+    {
+        public MyCustomException()
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            var message = Message;
+            return message;
+        }
+    }
+}
